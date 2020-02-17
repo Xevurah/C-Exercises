a89edf06700bcbd0154afd44788a6f6eb943ec47
Eduardo Alí Garcia <47980225+Xevurah@users.noreply.github.com>
2020-02-17 13:00:43 -0600

    WF button y Dynamicbutton

---
+++ Ejercicio 9 WF Ejemplo boton/Ejercicio 9 WF Ejemplo boton/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Ejercicio_9_WF_Ejemplo_boton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SpringGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(600, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Cerrar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ejemplo de botones";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ejercicio 9 WF Ejemplo boton/Ejercicio 9 WF Ejemplo boton/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_9_WF_Ejemplo_boton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateDynamicButton();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateDynamicButton()
+        {
+            //instancia nuevo objeto clase botón
+            Button dynamicButton = new Button();
+
+            //definir las propiedades del botón
+            //altura y ancho
+            dynamicButton.Height = 150;
+            dynamicButton.Width = 300;
+
+            //colores de fondo y fuente
+            dynamicButton.BackColor = Color.Blue;
+            dynamicButton.ForeColor = Color.Yellow;
+
+            //definir la posición del botón
+            dynamicButton.Location = new Point(20, 150);
+
+            //texto del botón
+            dynamicButton.Text = "I am a Dynamic button";
+
+            //estilo y tamaño de la fuente del texto
+            dynamicButton.Font = new Font("Georgia", 16);
+
+            //nombre del botón
+            dynamicButton.Name = "DynamicButton";
+
+            //agregar una imagen dentro del botón
+            dynamicButton.Image = Image.FromFile(@"C:\Users\ali_5\Pictures\dino.png");
+            //C:\Users\ali_5\Pictures\dino.png
+
+            //agregar un controlador 
+            dynamicButton.Click += new EventHandler(DynamicButton_Click);
+
+            //añadir y colocar el botón en el formulario
+            Controls.Add(dynamicButton);
+        }
+
+        private void DynamicButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dynamic Button is clicked");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Static button is clicked");
+            this.Close();
+        }
+    }
+}
